3c5b4abceb65a30d6bf89f33bd2de437fdf5a1a1
Stefanie Lima <stefanielima_araujo@hotmail.com>
2020-06-10 21:56:53 -0300

    change layout

--- 2020-Janeiro-Leitura-Organica/stelima/src/App.js
+++ 2020-Janeiro-Leitura-Organica/stelima/src/App.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react';
+
 import './global.css'
 import './App.css'
 import Timer from './components/Timer'
-import header from './utils/leitura-organica-logo.png'
+import text from './text'
+import { ReactComponent as HeaderImage } from './utils/header.svg'
+import { ReactComponent as SectionImage } from './utils/section.svg'
+import { ReactComponent as SectionImage2 } from './utils/section.svg'
 
 function App() {
   const { ms, start, pause, stop, running, result} = Timer();
@@ -26,34 +30,51 @@ function App() {
    if(result !== 0) {
     const minutes = result/60
     setPPM(words/minutes)
-    console.log('PPM', ppm)
    }
  }
 
   return (
-    <div className="App">
-      <img src={header} className="banner" alt="banner"/>
-      <div className="main">
+    <div className='App'>
+      <div className='menu'>Leitura Orgânica</div>
+      <header>
+      <h1>Você já parou para calcular a sua velocidade de leitura?</h1>
+      <HeaderImage className='header-image' />
+      </header>
+      <div className='main'>
         <section>
-          <h1>Você já parou para calcular a sua velocidade de leitura?</h1>
-          <h3>Num mundo tão subjetivo quanto o da leitura, a velocidade de leitura é uma métrica objetiva que te permite saber como está a sua leitura nesse momento e te permite acompanhar a sua evolução!
-    A medida mais utilizada para calcular a velocidade de leitura é Palavras por Minuto (PPM). Uma leitura em páginas por hora, por exemplo, não pode ser aplicada em qualquer livro (pois tem tamanhos diferentes de páginas), muito menos em artigos ou notícias.
-    Que tal descobrir agora a sua velocidade de leitura?</h3>
-          <h3>Para realizar o teste clique em Iniciar e leia o texto abaixo, quando terminar clique em Finalizar</h3>
+        <SectionImage className='section-image' />
+          <div class='description'>
+          <h2>O que é?</h2>
+            <p>Num mundo tão subjetivo quanto o da leitura, a velocidade de leitura é uma métrica objetiva que te permite saber como está a sua leitura nesse momento e te permite acompanhar a sua evolução!</p>
+          </div>
         </section>
-        <div className="timer">
+
+        <section>
+          <div class='description'>
+            <h2>Como funciona?</h2>
+            <p>A medida mais utilizada para calcular a velocidade de leitura é Palavras por Minuto (PPM). Uma leitura em páginas por hora, por exemplo, não pode ser aplicada em qualquer livro (pois tem tamanhos diferentes de páginas), muito menos em artigos ou notícias.
+            Que tal descobrir agora a sua velocidade de leitura?</p>
+          </div>
+          <SectionImage2 className='section-image' />
+        </section>
+        
+        <div className='timer'>
+        <p>Para realizar o teste clique em Iniciar e leia o texto abaixo, quando terminar clique em Finalizar</p>
           <button onClick={start}>Iniciar Teste</button>
           <h1>{Math.floor(ms / 1000)}s</h1>
         </div>
         <p className='text'>
-          “- Por que você, meu caro, apareceu tão de repente... O que está acontecendo? – perguntou ela, olhando para o penhor. - É uma cigarreira de prata: eu não lhe falei da outra vez? Ela estendeu a mão. - E por que é que você está tão pálido? Veja como as mãos estão tremendo! Tomou banho, meu caro? - É febre – respondeu com voz entrecortada. – Fica-se pálido a contragosto... quando não tem o que comer – acrescentou ele, mal pronunciando as palavras. Mais uma vez as forças o abandonaram. Mas a resposta pareceu verossímil; a velha pegou o penhor na mão. - Um objeto... uma cigarreira... de prata... dê uma olhada. - Que coisa, como se não fosse de prata... E como você amarrou! Procurando desamarrar o cadarço e voltando para janela, no sentido da claridade (todas as janelas estavam fechadas, apesar do abafamento), ela o deixou inteiramente por alguns segundos e lhe deu as costas. Ele desabotoou o sobretudo e soltou o machado de laço, mas ainda não o tirou por inteiro, ficando apenas a segurá-lo com a mão direita por cima da roupa. Os braços estavam terrivelmente fracos; ele mesmo os sentia a cada instante cada vez mais entorpecidos e duros. Temia soltar e deixar cair o machado... num repente foi como se a cabeça começasse a rodar. - O que foi que ele enrolou aqui! – gritou a velha irritada e mexeu-se na direção dele. Ele não podia perder nem mais um instante. Tirou o machado por inteiro, levantou-o com as duas mãos, mal se dando conta de si, e quase sem fazer força, quase maquinalmente, baixou-o de costas na cabeça dela. Era como se nesse instante tivesse lhe faltado força. A velha, como sempre, estava de cabeça descoberta. Os cabelos claros com tons grisalhos, ralinhos, habitualmente besuntados de óleo, formavam uma trança à moda rabo de rato e estavam presos a um resto de pente de chifre que se destacava na nuca. O golpe acertara em plenas têmporas, para o que contribuía a sua baixa estatura." 
+          {text.text[0]}
         </p>
-        <div className="timer">
+        <div className='timer'>
             <button onClick={pause}>Finalizar</button>
             {result > 0 ? <button onClick={stop}>Refazer Teste</button> : ''}
             {result > 0 ? <h1>Seu PPM é {ppm.toFixed(0)}</h1> : ''}
         </div>
       </div>
+      <footer>
+        <h4>Desenvolvido por <a href='https://github.com/stelima'>@stelima</a></h4>
+      </footer>
     </div>
   );
 }
